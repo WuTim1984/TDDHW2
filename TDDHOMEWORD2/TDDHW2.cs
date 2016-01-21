a16020c33fa9ba8c9e1971887e8617d4ae0ae8cb
wuchienting@gmail.com <wuchienting@gmail.com>
2016-01-21 12:38:26 +0800

    CASE1:紅燈 Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ TDDHOMEWORD2/TDDHW2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDHOMEWORD2
+{
+    public class TDDHW2 
+    {
+        public double Accounting(List<Book> booklist)
+        {  
+            double sum = 0;
+            return sum;
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ TDDHOMEWORD2Tests/TDDHW2Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TDDHOMEWORD2.Tests
+{
+    [TestClass()]
+    public class TDDHW2Tests
+    {
+        [TestMethod()] //case1
+        public void AccountingTest_id_1_quantity_1_shouldbe_100()
+        {  
+            //arrange
+            List<Book> book = new List<Book>() {
+            new Book{Id=1,Quantity = 1},
+            new Book{Id=2,Quantity = 0},
+            new Book{Id=3,Quantity = 0},
+            new Book{Id=4,Quantity = 0},
+            new Book{Id=5,Quantity = 0},
+            };
+
+            var target = new TDDHW2();
+            double actual = 0;
+            int expected = 100;
+            //act
+            actual = target.Accounting(book);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
